d2f01a4473a976272aaccfb9e708cfe4ee974eab
Ewerton Bruno <ewerton12042006@gmail.com>
2026-09-02 20:20:48 -0300

    insert department implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -24,6 +24,11 @@ public class Program2 {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TEST 3: department insert ===");
+		Department newDepartment = new Department(null, "Food");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
+		
 	}
 
 }
